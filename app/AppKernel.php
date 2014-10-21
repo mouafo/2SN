b8545904e061733ec787cd4a231e848eeee115d2
jbouhier <jb.bouhier@novactive.com>
2014-10-21 19:37:48 +0200

    Creation de Quelp/ImageBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
 	    new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Backend\AdminBundle\BackendAdminBundle(),
+            new Quelp\ImageBundle\QuelpImageBundle(),
 	  );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
