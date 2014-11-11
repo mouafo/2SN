fbb03a5552fd387d3dfc5ec3260a5e5781890d67
jbouhier <jb.bouhier@novactive.com>
2014-11-11 22:44:13 +0100

    [ImageBundle]: Updated entity Multimedia & config.yml to use VichUploaderBundle

--- src/DB/Bundle/dbBundle/Entity/Multimedia.php
+++ src/DB/Bundle/dbBundle/Entity/Multimedia.php
@@ -3,12 +3,15 @@
 namespace DB\Bundle\dbBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Multimedia
  *
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="DB\Bundle\dbBundle\Entity\MultimediaRepository")
+ * @Vich\Uploadable
  */
 class Multimedia
 {
@@ -65,11 +68,77 @@ class Multimedia
      * @ORM\ManyToOne(targetEntity="DB\Bundle\dbBundle\Entity\Album", cascade={"persist"})
      * @ORM\JoinColumn(nullable=false) */
     private $album;
-     
-    
+
+    /**
+     * @Vich\UploadableField(mapping="user_images", fileNameProperty="imageName")
+     *
+     * @note This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @var File $imageFile
+     */
+    protected $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="image_name")
+     *
+     * @var string $imageName
+     */
+    protected $imageName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime $updatedAt
+     */
+    protected $updatedAt;
+
     public function __construct() {
         $this->setCreateDate(new \Datetime());
         $this->setEditDate($this->getCreateDate());
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    /**
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param string $imageName
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
     }
 
     /**
